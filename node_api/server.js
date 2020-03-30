4f1c4d6b9243104de4e439360976c7708c373f01
Ishan Gupta <ishangupta004@gmail.com>
2020-03-30 10:02:24 +0530

    mongodb configured

--- node_api/server.js
+++ node_api/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.get("/", (req, res) => res.send("Root path of the app"));
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => console.log("MongoDB Connected Succesfully"))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
