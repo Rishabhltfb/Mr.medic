9b8dba6e9c026f9de6c817253b62e88994cd1d66
naveen-ku <naveen2951996@gmail.com>
2020-03-30 21:07:45 +0530

    Changing erroroneous class to className and adding landingpage cards

--- react_webapp/src/components/PatientLogin/PatientLogin.js
+++ react_webapp/src/components/PatientLogin/PatientLogin.js
@@ -4,16 +4,16 @@ export default class PatientLogin extends Component {
   render() {
     return (
       <div>
-        <div class="col-md-9 col-lg-8 mx-auto">
-          <h3 class="login-heading mb-4" style={{ color: "white" }}>
+        <div className="col-md-9 col-lg-8 mx-auto">
+          <h3 className="login-heading mb-4" style={{ color: "white" }}>
             Welcome back!
           </h3>
           <form>
-            <div class="form-label-group">
+            <div className="form-label-group">
               <input
                 type="email"
                 id="inputEmail"
-                class="form-control"
+                className="form-control"
                 placeholder="Email address"
                 required
                 autofocus
@@ -21,11 +21,11 @@ export default class PatientLogin extends Component {
               <label for="inputEmail">Email address</label>
             </div>
 
-            <div class="form-label-group">
+            <div className="form-label-group">
               <input
                 type="password"
                 id="inputPassword"
-                class="form-control"
+                className="form-control"
                 placeholder="Password"
                 required
               />
@@ -33,13 +33,13 @@ export default class PatientLogin extends Component {
             </div>
 
             <button
-              class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2"
+              className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2"
               type="submit"
             >
               Sign in
             </button>
-            <div class="text-center">
-              <a class="small" href="#">
+            <div className="text-center">
+              <a className="small" href="#">
                 Forgot password?
               </a>
             </div>
---
+++ react_webapp/src/views/LandingCards/LandingCards.js
@@ -0,0 +1,210 @@
+import React from "react";
+
+export default function LandingCards() {
+  return (
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="card mb-5 mb-lg-0">
+              <div className="card-body">
+                <h5 className="card-title text-muted text-uppercase text-center">
+                  Free
+                </h5>
+                <h6 className="card-price text-center">
+                  $0<span className="period">/month</span>
+                </h6>
+                <hr />
+                <ul className="fa-ul">
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Single User
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    5GB Storage
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Unlimited Public Projects
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Community Access
+                  </li>
+                  <li className="text-muted">
+                    <span className="fa-li">
+                      <i className="fas fa-times"></i>
+                    </span>
+                    Unlimited Private Projects
+                  </li>
+                  <li className="text-muted">
+                    <span className="fa-li">
+                      <i className="fas fa-times"></i>
+                    </span>
+                    Dedicated Phone Support
+                  </li>
+                  <li className="text-muted">
+                    <span className="fa-li">
+                      <i className="fas fa-times"></i>
+                    </span>
+                    Free Subdomain
+                  </li>
+                  <li className="text-muted">
+                    <span className="fa-li">
+                      <i className="fas fa-times"></i>
+                    </span>
+                    Monthly Status Reports
+                  </li>
+                </ul>
+                <a href="#" className="btn btn-block btn-primary text-uppercase">
+                  Button
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="card mb-5 mb-lg-0">
+              <div className="card-body">
+                <h5 className="card-title text-muted text-uppercase text-center">
+                  Plus
+                </h5>
+                <h6 className="card-price text-center">
+                  $9<span className="period">/month</span>
+                </h6>
+                <hr />
+                <ul className="fa-ul">
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    <strong>5 Users</strong>
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    50GB Storage
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Unlimited Public Projects
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Community Access
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Unlimited Private Projects
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Dedicated Phone Support
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Free Subdomain
+                  </li>
+                  <li className="text-muted">
+                    <span className="fa-li">
+                      <i className="fas fa-times"></i>
+                    </span>
+                    Monthly Status Reports
+                  </li>
+                </ul>
+                <a href="#" className="btn btn-block btn-primary text-uppercase">
+                  Button
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title text-muted text-uppercase text-center">
+                  Pro
+                </h5>
+                <h6 className="card-price text-center">
+                  $49<span className="period">/month</span>
+                </h6>
+                <hr />
+                <ul className="fa-ul">
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    <strong>Unlimited Users</strong>
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    150GB Storage
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Unlimited Public Projects
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Community Access
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Unlimited Private Projects
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Dedicated Phone Support
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    <strong>Unlimited</strong> Free Subdomains
+                  </li>
+                  <li>
+                    <span className="fa-li">
+                      <i className="fas fa-check"></i>
+                    </span>
+                    Monthly Status Reports
+                  </li>
+                </ul>
+                <a href="#" className="btn btn-block btn-primary text-uppercase">
+                  Button
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
