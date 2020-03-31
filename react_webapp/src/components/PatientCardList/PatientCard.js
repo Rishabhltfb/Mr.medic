cdb7287be2c5c9e2aa7a4540f9482128a1d725b4
naveen-ku <naveen2951996@gmail.com>
2020-03-31 22:11:11 +0530

    Fix QR code

--- react_webapp/src/components/PatientCardList/PatientCard.js
+++ react_webapp/src/components/PatientCardList/PatientCard.js
@@ -4,10 +4,13 @@ import QRCode from 'qrcode.react'
  
 
 const PatientCard = ({ name, email, id, city, address, avatar}) => {
+    const token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlODMwNWFmMjBlNjJjMTNkMDc3Y2NlOCIsIm5hbWUiOiJwYXRpZW50IiwiZW1haWwiOiJwYXRpZW50QGVtYWlsLmNvbSIsImF2YXRhciI6Ii8vd3d3LmdyYXZhdGFyLmNvbS9hdmF0YXIvYjE3ZmQ0M2FmMWFhZjhmNTRlZmUyMmIzMDFiNWFmNmQ_cz0yMDAmcj1wZyZkPW1tIiwiaWF0IjoxNTg1NjY4NTgyLCJleHAiOjE1ODU3MDQ1ODJ9.gnKLB2J-9UD99ZrPg8IFCjzKDCqm94FSQ5kE1CFbVBE"
   return (
-      <a href={`/patients/patient${id}`} target="_blank" style={{textDecoration: "none", color:"black"}}>
+      <a href={`/`} target="_blank" style={{textDecoration: "none", color:"black"}}>
     <div className="card mb-5 mt-5 row">
-    <div className="ml-5 mt-5 mx-auto"><QRCode value={id} /></div>
+    <div className="ml-5 mt-5 mx-auto">
+    <QRCode value={`${id},${token}`} renderAs="canvas"/>
+    </div>
       <div className="mx-auto mt-4">
         <h5 className="card-title mt-5 mx-auto">{name}</h5>
       </div>
